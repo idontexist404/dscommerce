156be6c6801e5500ab5060269968b124195e1753
highlander <highlander9200@gmail.com>
2023-12-07 09:56:05 -0300

    Add Order entity with the needed annotations to create a order table in the db

--- src/main/java/com/highlander/dscommerce/entities/User.java
+++ src/main/java/com/highlander/dscommerce/entities/User.java
@@ -1,14 +1,26 @@
 package com.highlander.dscommerce.entities;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
 
@@ -72,4 +84,7 @@ public class User {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
 }
